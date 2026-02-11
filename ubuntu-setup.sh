47eaad4fb4a806152c2491c6b4cad5f707c1ff8d
JavaCommons Technologies <javacommons@gmail.com>
2026-02-11 09:30:00 +0900

    .

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -5,5 +5,7 @@ cd "$(dirname "$0")"
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 sudo apt update
-sudo apt install -y mono-complete libgtk2.0-dev
+sudo apt install -y emacs
 sudo apt install -i gnome-terminal
+sudo apt install -y dotnet-sdk-10.0
+sudo apt install -y mono-complete libgtk2.0-dev
