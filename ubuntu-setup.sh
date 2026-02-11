e84f8111c1bc0a714b891e9a9b66a505faedcb57
JavaCommons Technologies <javacommons@gmail.com>
2026-02-11 09:39:59 +0900

    .

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,6 +6,8 @@ cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 sudo apt update
 sudo apt install -y emacs
-sudo apt install -i gnome-terminal
+sudo apt install -y gnome-terminal
 sudo apt install -y dotnet-sdk-10.0
 sudo apt install -y mono-complete libgtk2.0-dev
+wget -nc -T 8 -O /tmp/dart.deb https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_3.11.0-1_amd64.deb
+sudo dpkg -i /tmp/dart.deb
